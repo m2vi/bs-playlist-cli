2fb065a7e5b6df2e4b46f31e7e42679ef39e40c5
m2vi <iuvo@gmx.at>
2021-07-26 11:58:31 +0200

    final

--- src/utils/genPlaylist.ts
+++ src/utils/genPlaylist.ts
@@ -1,6 +1,8 @@
 import { GenPlaylistProps, PlaylistProps, SongProps } from 'src/types';
 import BeatSaverAPI from 'beatsaver-api';
 import { getSong } from './getSong';
+import { kowalski } from './image';
+import { plain, succ, err } from './log';
 
 export const api = new BeatSaverAPI({
   AppName: 'bs-playlist-cli',
@@ -13,22 +15,33 @@ export const genPlaylist = async ({
   description,
   songs,
 }: GenPlaylistProps) => {
-  let Songs: SongProps[] = [];
+  try {
+    let Songs: SongProps[] = [];
 
-  for (const key of songs) {
-    const song = await getSong(api, key);
-    if (song) Songs.push(song);
+    plain('Started generating playlist\n');
+
+    for (const key of songs) {
+      const song = await getSong(api, key);
+      if (song) Songs.push(song);
+    }
+
+    plain('Started adding playlist details');
+
+    let playlist: PlaylistProps = {
+      playlistTitle: title,
+      playlistAuthor: author,
+      playlistDescription: description ? description : 'Random Playlist',
+      songs: Songs,
+      image: kowalski,
+    };
+
+    succ('Finished playlist');
+
+    return playlist;
+  } catch (e) {
+    err(`Generating playlist failed (${e.message})`);
+    return null!;
   }
-
-  let playlist: PlaylistProps = {
-    playlistTitle: title,
-    playlistAuthor: author,
-    playlistDescription: description ? description : 'Random Playlist',
-    songs: Songs,
-    image: '',
-  };
-
-  return playlist;
 };
 
 export default genPlaylist;
--- src/utils/save.ts
+++ src/utils/save.ts
@@ -1,14 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import { SaveProps } from '../types';
-import { err } from './log';
+import { err, plain, succ } from './log';
 
 export const save = ({ dir, playlist }: SaveProps) => {
   try {
+    plain('Started saving the playlist');
     fs.writeFileSync(
       path.join(dir ? dir : __dirname, `/${playlist.playlistTitle}.bplist`),
       JSON.stringify(playlist, undefined, 2)
     );
+    succ(`Playlist saved to ${dir}`);
   } catch (error) {
     err("Couldn't save the playlist");
   }
